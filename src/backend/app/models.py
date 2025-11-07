69348b01c6e567e87acae05110cfdb57e7d1ab8a
michaeldye-edu <m.dye1@andersonuniversity.edu>
2025-11-07 15:38:14 -0500

    Changed models and seeding to allow for passwords

--- src/backend/app/models.py
+++ src/backend/app/models.py
@@ -14,6 +14,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False)
     name = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
     bookings = db.relationship("Booking", back_populates="user", cascade="all, delete-orphan")
